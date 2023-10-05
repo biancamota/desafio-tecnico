bb54ce244e02dc0a42f97009c08ba05029f80b8b
Bianca Mota <biancamota1209@gmail.com>
2023-10-04 21:46:38 -0300

    refactor: product list

--- frontend/src/pages/product/table.js
+++ frontend/src/pages/product/table.js
@@ -1,10 +1,9 @@
 import { ref } from 'vue'
 
 const columnsProduct = [
-  { name: 'img_url', align: 'left', label: 'Img', field: 'img_url', sortable: false },
   { name: 'name', align: 'left', label: 'Name', field: 'name', sortable: true },
-  { name: 'amount', align: 'left', label: 'Amount', field: 'amount', sortable: true },
   { name: 'price', align: 'left', label: 'Price', field: 'price', sortable: true },
+  { name: 'category_id', align: 'left', label: 'Category', field: 'category_id', sortable: true },
   { name: 'actions', align: 'right', label: 'Actions', field: 'actions', sortable: false }
 ]
 
