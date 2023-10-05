aa11ff57bfd00577edaaf18073b3661eb128e5b8
Bianca Mota <biancamota1209@gmail.com>
2023-10-04 23:18:42 -0300

    refactor: add taxe column

--- frontend/src/pages/category/table.js
+++ frontend/src/pages/category/table.js
@@ -1,5 +1,6 @@
 const columnsCategory = [
   { name: 'name', align: 'left', label: 'Name', field: 'name', sortable: true },
+  { name: 'taxe', align: 'left', label: 'Taxe', field: 'taxe', sortable: true },
   { name: 'actions', align: 'right', label: 'Actions', field: 'actions', sortable: true }
 ]
 
